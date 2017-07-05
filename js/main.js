3e8fcd6a75d7fc37149b8fbcb53e1cbe73ef15bd
Aktilek <aktilek.sof@gmail.com>
2017-07-05 06:25:36 +0600

    Finish

--- js/main.js
+++ js/main.js
@@ -27,15 +27,15 @@ $('.slider-order').slick({
     responsive: [
          {
                 settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 1,
+                slidesToScroll: 1
             }
         },
         {
-            breakpoint: 1000,
+            breakpoint: 320,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 1,
+                slidesToScroll: 1
             }
         }
 
